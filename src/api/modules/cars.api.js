0873137c36830f8b8e26428983af16eb6d6615d0
PetrKopyev <petrkopev@gmail.com>
2022-05-18 22:21:38 +0400

    F09/F10 Реализация создания/изменения/удаления автомобиля

--- src/api/modules/cars.api.js
+++ src/api/modules/cars.api.js
@@ -1,8 +1,20 @@
 import httpClient from '@/api/httpClient';
 import { CARS_PER_PAGE } from '@/constants/cars.contants';
 
+const getCar = (id) => httpClient.get(`/db/car/${id}`);
+
 const getCars = ({ page, limit = CARS_PER_PAGE, categoryId = null }) => httpClient.get('/db/car', { params: { page, limit, categoryId } });
 
+const createCar = (car) => httpClient.post('/db/car', { ...car });
+
+const updateCar = (id, car) => httpClient.put(`/db/car/${id}`, { ...car });
+
+const deleteCar = (id) => httpClient.delete(`/db/car/${id}`);
+
 export default {
+  getCar,
   getCars,
+  createCar,
+  updateCar,
+  deleteCar,
 };
--- src/store/modules/cars.js
+++ src/store/modules/cars.js
@@ -19,6 +19,18 @@ export default {
   },
 
   actions: {
+    async fetchCar(_, id) {
+      try {
+        const { data } = await api.cars.getCar(id);
+
+        return Promise.resolve(data.data);
+      } catch (e) {
+        console.error(e);
+
+        return Promise.reject();
+      }
+    },
+
     async fetchCars({ commit }, { page = 1, categoryId = null }) {
       try {
         const { data } = await api.cars.getCars({ page: page - 1, categoryId });
